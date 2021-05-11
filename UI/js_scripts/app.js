89984cca5c14a4ec02f59ad21b6e90bbe470b2a7
jhinnen1 <70825145+jhinnen1@users.noreply.github.com>
2021-05-10 23:09:53 -0500

    clean-up

--- UI/js_scripts/app.js
+++
@@ -1,5 +0,0 @@
-function onLoad() {
-  //alert("Function onLoad() called");
-  document.getElementById("timestamp").innerHTML = Date();
-}
-
--- UI/render.js
+++
@@ -1,5 +0,0 @@
-function renderHello() {
-  var template = document.getElementById('template').innerHTML;
-  var rendered = Mustache.render(template, { name: 'Patron' });
-  document.getElementById('target').innerHTML = rendered;
-}
